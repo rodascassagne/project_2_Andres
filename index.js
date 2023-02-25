f27002754b2d09c8f1685e578d6e8ffcaa7ee255
yuyo <rodascassagne@gmail.com>
2023-02-25 17:59:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/doble/:num', (req, res) => {
+    console.log(req.params);
+    const doble = {
+        "num": req.params.num,
+        "resultado": req.params.num * 2
+    }
+    res.json(doble)
+})
+
+
+
+
+
+app.get('/estudiantes', (req, res) => {
+    const estudiantes = [
+        {
+            "nombre": "Pedro Campos",
+            "correo": "pedro@correo.de"
+        },
+        {
+            "nombre": "Jimena Mora",
+            "correo": "jimena@correo.de"
+        }
+    ]
+    res.json(estudiantes);
+})
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
